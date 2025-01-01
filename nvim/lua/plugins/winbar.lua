c89a75fc91ffb02031192e17449b20cead867dbb
nitishxyz <nitishxyz@proton.me>
2025-01-01 18:04:09 +0530

    using barbecue.nvim

--- nvim/lua/plugins/winbar.lua
+++ nvim/lua/plugins/winbar.lua
@@ -1,12 +1,13 @@
-return {
-  "fgheng/winbar.nvim",
-  config = function()
-    require('winbar').setup({
-      enabled = true,
-      options = {
-        show_file_path = true,
-        show_symbols = true,
-      }
-    })
-  end
-}
+return {}
+-- return {
+--   "fgheng/winbar.nvim",
+--   config = function()
+--     require('winbar').setup({
+--       enabled = true,
+--       options = {
+--         show_file_path = true,
+--         show_symbols = true,
+--       }
+--     })
+--   end
+-- }
