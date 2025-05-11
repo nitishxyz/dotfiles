dee1fe5ad45ce04003afc49f0c4a2edd32202a9c
nitishxyz <nitishxyz@proton.me>
2025-05-12 02:05:21 +0530

    Feat: Add tmux pane navigation
    Adds vim-tmux-navigator plugin to seamlessly navigate between vim and tmux panes using Ctrl+hjkl.
    
    This also removes the custom pane navigation keymaps from options.lua and instead configures the plugin to handle the navigation. This ensures consistent behavior between vim and tmux.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -18,4 +18,17 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
-vim.opt.sidescrolloff = 8  -- Prevents horizontal scrolling until cursor is 8 chars from edge 
+vim.opt.sidescrolloff = 8 -- Prevents horizontal scrolling until cursor is 8 chars from edge
+
+local map = vim.api.nvim_set_keymap
+vim.opt.termguicolors = true
+local opts = { noremap = true, silent = true }
+
+-- Navigate vim panes better
+-- Move cursor to top/bottom pane with Ctrl-K/J respectively,
+-- and move cursor to previous/next pane with Ctrl-H/L respectively.
+map("n", "<c-k>", ":wincmd k<CR>", opts)
+map("n", "<c-j>", ":wincmd j<CR>", opts)
+map("n", "<c-h>", ":wincmd h<CR>", opts)
+map("n", "<c-l>", ":wincmd l<CR>", opts)
+
